48be51536bc3ad4aea706c6ef1dbe11f2f74eb55
ChandaRoy <chandasays2me@gmail.com>
2016-08-30 17:09:36 +0530

    working multiseries with checkboxes

--- public/components/Reports/report.controller.js
+++ public/components/Reports/report.controller.js
@@ -1,7 +1,7 @@
 myApp.controller('reportController', ['$scope', '$state', '$rootScope', 'reportService', function($scope, $state, $rootScope, reportService) {
 
   $scope.params=["Temperature","Humidity","Current","Voltage","KW","H2"];
-  $scope.selection = ["Humidity","Current","Voltage","KW","H2"];
+  $scope.selection = ["Temperature","Humidity","Current","Voltage","KW","H2"];
   $scope.toggleSelection = function toggleSelection(param) {
     var idx = $scope.selection.indexOf(param);
     // is currently selected
@@ -37,7 +37,7 @@ myApp.directive('multiSeries', function(){
   padding = 100;
 
   return{
-    restrict:'E',
+    restrict:'EA',
     scope:{
       data:'=',
       checked:'='
@@ -48,6 +48,10 @@ myApp.directive('multiSeries', function(){
       .append("svg")
       .attr("width", width)
       .attr("height", height + 100);
+      var selection=scope.checked;
+      var data;
+      var domain=100;
+
 
       //       scope.$watch('checked',function(new,old){
       // if(!new) return;
@@ -55,13 +59,13 @@ myApp.directive('multiSeries', function(){
       //       console.log(new);
       //       })
       console.log(scope);
-      scope.$watchCollection('[data,checked]', function (newVal, oldVal) {
+      scope.$watch('data', function (newVal, oldVal) {
         svg.selectAll('*').remove();
         if (!newVal) {
+          console.log(newVal, oldVal);
           return;
         }
-        var data=JSON.parse(newVal[0]);
-        var selection=newVal[1];
+        data=JSON.parse(newVal);
         var temperature = [];
         var humidity = [];
         var h2=[];
@@ -76,9 +80,26 @@ myApp.directive('multiSeries', function(){
           current.push({"parameter":"current","value":data[i].current,"date":data[i].date});
           kw.push({"parameter":"kw","value":data[i].kw,"date":data[i].date});
         }
-        console.log(attrs.checked);
-        records(temperature, humidity,h2,voltage,current,kw,selection);
-      })
+        //console.log(selection);
+        scope.$watchCollection('checked',function(nv,ov){
+          if(!nv) return;
+          selection=nv;
+          console.log(selection);
+          change(selection);
+          if(selection.length==1)
+          {
+console.log(selection.length);
+
+            var val=selection[0].toLowerCase();
+
+          domain=d3.max(d3.values(val))
+console.log(domain);
+          }
+        })
+
+        records(temperature, humidity,h2,voltage,current,kw,selection,domain);
+
+      }, true);
 
 
 
@@ -86,7 +107,7 @@ myApp.directive('multiSeries', function(){
 
 
 
-      function records(temperature, humidity,h2,voltage,current,kw,selection) {
+      function records(temperature, humidity,h2,voltage,current,kw,selection,domain) {
 
 
         //Create the Scale we will use for the Axis
@@ -101,7 +122,7 @@ myApp.directive('multiSeries', function(){
 
         //Create the Scale we will use for the Axis
         var yAxisScale = d3.scale.linear()
-        .domain([100, 0])
+        .domain([domain, 0])
         .range([0, 400]);
         //Create the Axis
         var yAxis = d3.svg.axis().orient("left").scale(yAxisScale);
@@ -109,11 +130,8 @@ myApp.directive('multiSeries', function(){
         //Create an SVG group Element for the Axis elements and call the xAxis function
         var yAxisGroup = svg.append("svg:g").attr("transform", "translate("+padding+",10)").call(yAxis);
 
-        // Define 'div' for tooltips
-        // var div = d3.select("#graph")
-        // .append("div")  // declare the tooltip div
-        // .attr("class", "tooltip")              // apply the 'tooltip' class
-        // .style("opacity", 0);                  // set the opacity to nil
+        //  Define 'div' for tooltips
+        // set the opacity to nil
 
         svg.append("text")
         .attr("class", "yaxis_label")
@@ -164,49 +182,115 @@ myApp.directive('multiSeries', function(){
         //The line SVG Path we draw
 
 
-          var temperatureGraph = svg.append("svg:path")
-          .attr("d", temperatureLine(temperature));
-          if(selection.indexOf("Temperature")!=-1)
-          {
-        temperatureGraph.attr("stroke", "blue")
-          .attr("stroke-width", 2)
-          .attr("fill", "none");
-        }
-else temperatureGraph.attr("display","none");
+        var temperatureGraph = svg.append("svg:path")
+        .attr("class", "temperature")
+        .attr("d", temperatureLine(temperature))
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none");
+        myParam(temperature);
 
 
         //The line SVG Path we draw
 
         var humidityGraph = svg.append("svg:path")
         .attr("d", humidityLine(humidity))
+        .attr("class", "humidity")
         .attr("stroke", "red")
         .attr("stroke-width", 2)
         .attr("fill", "none");
+        myParam(humidity);
+
 
         var currentGraph = svg.append("svg:path")
         .attr("d", currentLine(current))
+        .attr("class", "current")
         .attr("stroke", "green")
         .attr("stroke-width", 2)
         .attr("fill", "none");
+        myParam(current);
+
         var voltageGraph = svg.append("svg:path")
         .attr("d", voltageLine(voltage))
+        .attr("class", "voltage")
         .attr("stroke", "orange")
         .attr("stroke-width", 2)
         .attr("fill", "none");
+        myParam(voltage)
+
         var h2Graph = svg.append("svg:path")
         .attr("d", h2Line(h2))
+        .attr("class", "h2")
         .attr("stroke", "purple")
         .attr("stroke-width", 2)
         .attr("fill", "none");
+        myParam(h2);
+
         var kwGraph = svg.append("svg:path")
         .attr("d", kwLine(kw))
+        .attr("class", "kw")
         .attr("stroke", "yellow")
         .attr("stroke-width", 2)
         .attr("fill", "none");
+        myParam(kw);
+
+
+        function myParam(data)
+        {
+          var p=data.parameter;
+          var div = d3.select("."+p)
+          .append("div")  // declare the tooltip div
+          .attr("class", "tooltip")              // apply the 'tooltip' class
+          .style("opacity", 0);
+
+
+          svg.selectAll("data.parameter")
+          .data(data)
+          .enter().append("circle")
+          .attr("r", 0)
+          .attr("cx", function(d) { return 100+(d.date-1)*(800/55); })
+          .attr("cy", function(d) { return (400-d.value*4)+10; })
+          .on("mouseover", function(d) {
+            div.transition()
+            .duration(200)
+            .style("opacity", .9);
+            div .html((d.date) + "<br/>"  + d.value)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+          })
+          .on("mouseout", function(d) {
+            div.transition()
+            .duration(500)
+            .style("opacity", 0);
+          });
+        }
+
+
+      }
+
+      function change(selection){
+        console.log("inside change");
+        if(selection.indexOf("Temperature")==-1)
+        d3.selectAll(".temperature").attr("visibility","hidden");
+        else if(selection.indexOf("Temperature")!=-1) {
+          d3.selectAll(".temperature").attr("visibility","visible");
+        }
+        if(selection.indexOf("KW")==-1) d3.selectAll('.kw').attr("visibility","hidden");
+        else if(selection.indexOf("KW")!=-1) {d3.selectAll('.kw').attr("visibility","visible"); }
+        if(selection.indexOf("H2")==-1) d3.selectAll('.h2').attr("visibility","hidden");
+        else if(selection.indexOf("H2")!=-1) {d3.selectAll('.h2').attr("visibility","visible");}
+        if(selection.indexOf("Voltage")==-1) d3.selectAll('.voltage').attr("visibility","hidden");
+        else if(selection.indexOf("Voltage")!=-1) {d3.selectAll('.voltage').attr("visibility","visible");}
+        if(selection.indexOf("Humidity")==-1) d3.selectAll('.humidity').attr("visibility","hidden");
+        else if(selection.indexOf("Humidity")!=-1) {d3.selectAll('.humidity').attr("visibility","visible"); }
+        if(selection.indexOf("Current")==-1) d3.selectAll('.current').attr("visibility","hidden");
+        else if(selection.indexOf("Current")!=-1) {d3.selectAll('.current').attr("visibility","visible"); }
+
 
 
 
       }
+
 
     }
   };
